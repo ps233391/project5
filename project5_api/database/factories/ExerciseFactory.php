c46254bdc58812b69778fbf01c35aac6076344b4
PS228140 <93662444+PS228140@users.noreply.github.com>
2023-06-15 11:13:32 +0200

    api met breeze

---
+++ project5_api/database/factories/ExerciseFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Faker\Guesser\Name;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\exercise>
+ */
+class ExerciseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            Name
+        ];
+    }
+}
